4ca4ca78e5446d62c4d355dfeb7f27cc366a4041
ariel <ariel10player@gmail.com>
2026-02-16 17:30:28 -0300

    feat: paleta de colores

---
+++ src/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+    theme: {
+        extend: {
+            colors: {
+                blanco: "#F8F2DC",
+                rojo: "#CD4631",
+                azul: "#81ADC8",
+                marronClaro: "#DEA47E",
+                marronOscuro: "#9E6240",
+            },
+        },
+    },
+}
