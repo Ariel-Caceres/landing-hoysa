e3ea720d161959c6fb94cbd32296df2a55141cfe
ariel <ariel10player@gmail.com>
2026-02-16 18:07:59 -0300

    feat; banner

---
+++ src/components/Banner.tsx
@@ -0,0 +1,11 @@
+export const Banner = () => {
+    return (
+        <div className="w-full flex justify-center bg-marron-oscuro text-white text-xl mt-5">
+            <div className="  items-center flex justify-between w-[90%]">
+                <span className="flex gap-2 items-center"><i className="fa-solid fa-truck-arrow-right "></i>Envíos gratis</span>
+                <span>Envíos gratis por la zona barrio La Escondida</span>
+                <span className="flex gap-2 items-center"><i className="fa-solid fa-truck-arrow-right"></i>Envíos Gratis</span>
+            </div>
+        </div>
+    )
+}
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,11 +2,13 @@ export default {
     theme: {
         extend: {
             colors: {
+                // Paleta de colores personalizada
                 blanco: "#F8F2DC",
                 rojo: "#CD4631",
                 azul: "#81ADC8",
                 marronClaro: "#DEA47E",
                 marronOscuro: "#9E6240",
+                // Agrega más colores aquí según necesites
             },
         },
     },
