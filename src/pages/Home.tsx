5e92da52105028355a3b5454a7b320636c6efc00
ariel <ariel10player@gmail.com>
2026-02-16 23:57:59 -0300

    feat: navBar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import { Banner } from "../components/Banner"
+import { NavBar } from "../components/NavBar"
 
 export const Home = () => {
     return (
         <div>
             <Banner />
+            <NavBar />
+
         </div>
     )
 }
