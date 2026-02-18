63597a480022c22e8ba5d56c5d9cd459b6a6259a
ariel <ariel10player@gmail.com>
2026-02-17 23:48:51 -0300

    feat: mainProduct component

---
+++ src/components/MainProduct.tsx
@@ -0,0 +1,9 @@
+export const MainProduct = () => {
+    return (
+        <div className="w-full flex justify-center items-center flex-col">
+            <div className="w-[85%] h-[50vh] flex ">
+                <img src="public/cpc-banner-2.png" alt="" className="w-full h-full" />
+            </div>
+        </div>
+    )
+}
