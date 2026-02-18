e68ac9ead32094361aad1a6778e43ba7701f7aab
ariel <ariel10player@gmail.com>
2026-02-18 02:36:56 -0300

    teaser, carrousel products

--- src/components/ProductsCarrousel.tsx
+++ src/components/ProductsCarrousel.tsx
@@ -1,22 +1,44 @@
 
+import { products } from "../data/products"
 
 export const ProductsCarrousel = () => {
+
     return (
         <div className="w-full justify-center items-center flex flex-col">
-            <div className=" flex w-[80%] justify-between">
-                <div className="text-3xl">
+            <div className=" flex md:w-[70%] w-[90%] justify-between py-6 items-center">
+                <div className="text-xl sm:text-3xl">
                     <span>Productos</span>
                 </div>
+                <div className="border-b-2 border-gray-400 block w-[70%]">
 
-
-                <div className="text-5xl gap-5 flex">
-                    <button className="">
+                </div>
+                <div className="xl:text-5xl md:text-3xl sm:text-3xl text-3xl gap-5 flex">
+                    <button className="cursor-pointer">
                         <i className="fa-regular fa-circle-left"></i>
                     </button>
-                    <button >
+                    <button className="cursor-pointer">
                         <i className="fa-regular fa-circle-right "></i>
-
                     </button>
+                </div>
+            </div>
+
+
+            <div className="w-[80%] ">
+                <div className="w-full flex justify-between">
+                    {products.map((p, i) =>
+                        <div className="w-[20%] flex flex-col bg-rojo overflow-hidden gap-5 p-3" key={i}>
+                            <div className="text-center py-2">
+                                <span>{p.nombre}</span>
+                            </div>
+                            <div className="rounded-[50%] overflow-hidden h-62.5 w-full">
+                                <img src={p.img} alt="" className="w-full h-full" />
+                            </div>
+                            <div>
+                                <span>{p.precio}</span>
+                            </div>
+                        </div>
+
+                    )}
                 </div>
             </div>
         </div>
