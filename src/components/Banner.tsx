a9fde394869134ad027cbf7376cf29126aa1ba53
ariel <ariel10player@gmail.com>
2026-02-18 00:17:09 -0300

    styles: banner free delivery

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,12 +2,17 @@ export const Banner = () => {
     return (
         <div className="w-full  justify-center  text-white text-sm flex pt-5 pb-5">
             <div className=" items-center flex justify-between w-[95%] bg-marron-oscuro p-2 rounded-2xl">
-                <span className=" gap-2 items-center md:flex hidden w-[25%]"><i className="fa-solid fa-truck-arrow-right "></i>Envíos gratis</span>
-                <div className="flex justify-center xl:w-[60%] w-full">
+                <div className="gap-2 items-center md:flex hidden justify-center w-[25%]">
+                    <i className="fa-solid fa-truck-arrow-right"></i>
+                    <span className=" ">Envíos Gratis</span>
+                </div>                <div className="flex justify-center xl:w-[60%] w-full">
                     <span className="text-md text-center xl:text-xl">Envíos gratis por la zona barrio La Escondida</span>
                 </div>
-                <span className=" gap-2 items-center md:flex hidden  w-[25%]"><i className="fa-solid fa-truck-arrow-right"></i>Envíos Gratis</span>
+                <div className="gap-2 items-center md:flex hidden justify-center w-[25%]">
+                    <i className="fa-solid fa-truck-arrow-right"></i>
+                    <span className=" ">Envíos Gratis</span>
+                </div>
             </div>
         </div>
     )
-}
+} 
