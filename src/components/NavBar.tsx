0708445ec79943e9010cbb1772edeb0b6daddadd
ariel <ariel10player@gmail.com>
2026-02-19 14:50:33 -0300

    style: navBar mobile responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react"
+
 export const NavBar = () => {
+    const [showNavbar, setShowNavBar] = useState<boolean>(false)
 
     return (
         <div className="w-full flex justify-center items-center md:py-2 ">
@@ -9,13 +12,24 @@ export const NavBar = () => {
                         <img src="images/logo-removebg.png" alt="" className="w-full" />
                     </div>
 
-                    <ul className="xl:text-[15px] md:text-[13px] xl:w-[45%]  md:w-[70%]  hidden md:flex justify-evenly p-3 text-[12px] xl:text-md w-full  xl:gap-4">
+                    <ul className={`shadow-black fixed w-64 md:static top-0 right-0  md:h-auto  md:w-[70%] xl:w-[45%] bg-blanco h-full md:bg-transparent md:border-none border-l rounded-l-3xl  flex flex-col md:flex-row gap-6 md:gap-0 xl:gap-4 p-6 md:p-3 shadow-xl md:shadow-none text-[16px] md:text-[13px] xl:text-[15px] justify-start md:justify-evenly transform transition-transform duration-300 ${showNavbar ? "translate-x-0" : "translate-x-[110%]"} md:translate-x-0 z-20`}>
+                        {showNavbar &&
+                            <>
+                                <div className="w-full flex justify-center border-b border-gray-400">
+                                    <img src="images/favicon.png" alt="" className="w-[50%] " />
+                                </div>
+                                <div className="absolute top-5 right-5 text-2xl" onClick={() => setShowNavBar(false)}>
+                                    <i className="fa-solid fa-xmark"></i>
+                                </div>
+
+                            </>
+                        }
                         <li className="hover:cursor-pointer text-center hover:text-rojo w-1/5 ">
                             <a href="https://wa.me/541171416222" target="_blank" className="animate-pulse" id="wsp">
                                 <span>Contacto</span>
                             </a>
                         </li>
-                        <li className="hover:cursor-pointer text-center hover:text-rojo min-w-36">
+                        <li className="hover:cursor-pointer text-center hover:text-rojo min-w-36  md:block flex ">
                             <span>Sobre Nosotros</span>
                         </li>
                         <li className="hover:cursor-pointer text-center hover:text-rojo w-1/4">
@@ -25,6 +39,10 @@ export const NavBar = () => {
                             <span>Productos</span>
                         </li>
                     </ul>
+
+                    <div className="text-2xl flex md:hidden cursor-pointer" onClick={() => setShowNavBar(true)}>
+                        <i className="fa-solid fa-bars"></i>
+                    </div>
                 </div>
 
                 <div className="hidden  items-center bg-white rounded-2xl md:justify-between md:w-[20%] w-[50%] justify-self-center">
