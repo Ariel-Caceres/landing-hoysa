4d5d0161055ce95a35c3b6688b0719bc7fabe3cb
ariel <ariel10player@gmail.com>
2026-02-19 17:24:17 -0300

    main product banner image change

--- src/components/MainProduct.tsx
+++ src/components/MainProduct.tsx
@@ -2,11 +2,11 @@ export const MainProduct = () => {
     return (
 
         <div className="w-full flex justify-center ">
-            <div className="w-[95%] xl:w-[80%]">
+            <div className="w-[95%] xl:w-[80%] ">
                 <img
-                    src="images/cpc-banner-3.png"
+                    src="images/cpc-banner-chopp.png"
                     alt="Main banner"
-                    className="w-full sm:h-50 h-37.5  md:h-100  object-[center_10%] object-cover rounded-3xl"
+                    className="w-full sm:h-50  md:h-70 lg:h-75 xl:h-90 object-[center_10%] object-fit rounded-3xl"
                 />
             </div>
         </div>
