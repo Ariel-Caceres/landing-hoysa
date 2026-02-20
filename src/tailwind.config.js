ac34a7dd5a745cd4c375713b044960c1b9356e25
ariel <ariel10player@gmail.com>
2026-02-20 17:42:49 -0300

    feat: new colors added

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -3,13 +3,17 @@ export default {
         extend: {
             colors: {
                 // Paleta de colores personalizada
+                blancoClaro: "#EAEADA",
                 blanco: "#F8F2DC",
+                blancoOscuro: "#F6E8D2",
                 rojo: "#CD4631",
                 azul: "#81ADC8",
                 marronClaro: "#DEA47E",
                 marronOscuro: "#9E6240",
+                marronOscuro2: "#623D28"
                 // Agrega más colores aquí según necesites
             },
+
         },
     },
 }
