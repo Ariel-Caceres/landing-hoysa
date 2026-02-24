c0e5efda88f83795c0aae252c5be9a8b3e8f41c4
ariel <ariel10player@gmail.com>
2026-02-24 07:10:16 -0300

    feat: location path for selected page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 export const NavBar = () => {
     const navigate = useNavigate()
     const [showNavbar, setShowNavBar] = useState<boolean>(false)
+    const location = useLocation()
+
 
     return (
         <div className="w-full flex justify-center items-center md:py-2 ">
@@ -13,26 +15,29 @@ export const NavBar = () => {
                         <img src="images/logo-removebg.png" alt="" className="w-full" />
                     </div>
 
-                    <ul className={` shadow-black fixed w-64 md:static  top-0 right-0 xl:text-3xl lg:text-md md:h-auto delay-200  lg:w-[50%]  md:w-[70%] xl:w-[55%] bg-blanco h-full md:bg-transparent md:border-none border-l rounded-l-3xl  flex flex-col md:flex-row gap-6 md:gap-0 xl:gap-4 p-6 md:p-3 shadow-xl md:shadow-none text-[16px] md:text-[13px] xl:text-[15px] justify-start md:justify-evenly transform transition-transform duration-300 ${showNavbar ? "translate-x-0" : "translate-x-[110%]"} md:translate-x-0 z-20`}>
+                    <ul className={` shadow-black fixed w-64 md:static  top-0 right-0 xl:text-3xl lg:text-md md:h-auto delay-200  lg:w-[40%]  md:w-[50%] xl:w-[35%]
+                         bg-blanco h-full md:bg-transparent md:border-none border-l rounded-l-3xl  flex flex-col md:flex-row gap-6 md:gap-0 xl:gap-4 p-6 md:p-3
+                          shadow-xl md:shadow-none text-[16px] md:text-[13px] xl:text-[15px] justify-start md:justify-evenly transform transition-transform duration-300 
+                          ${showNavbar ? "translate-x-0" : "translate-x-[110%]"} md:translate-x-0 z-20`}>
                         <div className="w-full md:hidden flex justify-center border-b border-gray-400 ">
                             <img src="images/favicon.png" alt="" className="w-[60%] " />
                         </div>
                         <div className="absolute md:hidden top-5 right-5 text-3xl active:rotate-45 transition-all ease-in-out duration-200 active:scale-75 cursor-pointer" onClick={() => setShowNavBar(false)}>
                             <i className="fa-solid fa-xmark"></i>
                         </div>
-
-                        <li className="hover:cursor-pointer text-center hover:text-rojo w-1/5 " onClick={() => navigate("/contact")}>
+                        <li className={`hover:cursor-pointer text-center hover:text-rojo w-1/4 ${location.pathname == "/productos" ? "text-rojo" : ""}`} onClick={() => navigate("/productos")}>
+                            <span>Productos</span>
+                        </li>
+                        <li className={`hover:cursor-pointer text-center hover:text-rojo w-1/5 ${location.pathname == "/contacto" ? "text-rojo" : ""}`} onClick={() => navigate("/contacto")}>
                             <span>Contacto</span>
                         </li>
-                        <li className="hover:cursor-pointer text-center hover:text-rojo min-w-36  md:block flex ">
+                        {/* <li className="hover:cursor-pointer text-center hover:text-rojo min-w-36  md:block flex ">
                             <span>Sobre Nosotros</span>
                         </li>
                         <li className="hover:cursor-pointer text-center hover:text-rojo w-1/4">
                             <span>Sabores</span>
-                        </li>
-                        <li className="hover:cursor-pointer text-center hover:text-rojo w-1/4">
-                            <span>Productos</span>
-                        </li>
+                        </li> */}
+
                         <div className="block border-b border-b-gray-400">
                         </div>
                     </ul>
