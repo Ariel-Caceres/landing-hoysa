183bac48d247362aa0896609d27c8c7757dc9652
ariel <ariel10player@gmail.com>
2026-02-24 03:52:41 -0300

    chore: logo click navigation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,11 +9,11 @@ export const NavBar = () => {
             <div className="w-[95%] flex md:justify-between items-center justify-center flex-wrap xl:w-[80%] pb-4">
                 <div className="flex items-center  md:w-[80%] xl:gap-8 lg:gap-3 flex-wrap wrap-normal xl:justify-start  md:justify-start justify-between w-full ">
 
-                    <div className="w-[40%] xl:w-[15%] lg:w-[20%] md:w-[20%] sm:w-[30%] rounded-2xl overflow-hidden  ">
+                    <div className="w-[40%] xl:w-[15%] lg:w-[20%] md:w-[20%] sm:w-[30%] rounded-2xl overflow-hidden  cursor-pointer" onClick={() => navigate("/")}>
                         <img src="images/logo-removebg.png" alt="" className="w-full" />
                     </div>
 
-                    <ul className={` shadow-black fixed w-64 md:static  top-0 right-0 lg:text-md md:h-auto  lg:w-[50%]  md:w-[70%] xl:w-[55%] bg-blanco h-full md:bg-transparent md:border-none border-l rounded-l-3xl  flex flex-col md:flex-row gap-6 md:gap-0 xl:gap-4 p-6 md:p-3 shadow-xl md:shadow-none text-[16px] md:text-[13px] xl:text-[15px] justify-start md:justify-evenly transform transition-transform duration-300 ${showNavbar ? "translate-x-0" : "translate-x-[110%]"} md:translate-x-0 z-20`}>
+                    <ul className={` shadow-black fixed w-64 md:static  top-0 right-0 xl:text-3xl lg:text-md md:h-auto  lg:w-[50%]  md:w-[70%] xl:w-[55%] bg-blanco h-full md:bg-transparent md:border-none border-l rounded-l-3xl  flex flex-col md:flex-row gap-6 md:gap-0 xl:gap-4 p-6 md:p-3 shadow-xl md:shadow-none text-[16px] md:text-[13px] xl:text-[15px] justify-start md:justify-evenly transform transition-transform duration-300 ${showNavbar ? "translate-x-0" : "translate-x-[110%]"} md:translate-x-0 z-20`}>
                         {showNavbar &&
                             <div className="md:hidden">
                                 <div className="w-full flex justify-center border-b border-gray-400 ">
