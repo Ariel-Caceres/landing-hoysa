f4db5442f8b43ce470c24bdeaa729d07dac05131
ariel <ariel10player@gmail.com>
2026-02-24 22:42:18 -0300

    fix: random focus while navigate

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Home } from './pages/Home';
 import { Contact } from './pages/Contact';
 import { Products } from './pages/Products';
-import { FocusReset } from './components/FocusReset';
+// import { FocusReset } from './components/FocusReset';
 
 function App() {
     return (
         <div className="bg-blanco xl:h-auto min-h-screen">
             <Router>
-                <FocusReset />
+                {/* <FocusReset /> */}
 
                 <Routes>
                     <Route path="/" element={<Home />} />
